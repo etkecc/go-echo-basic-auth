6a69d27aab0d66d6a377bd6dfa893fb597e9df6c
Aine <aine@etke.cc>
2023-11-24 22:20:36 +0200

    init

---
+++ auth_test.go
@@ -0,0 +1,34 @@
+package echobasicauth
+
+import (
+	"testing"
+)
+
+func TestNewValidator(t *testing.T) {
+	auth := &Auth{Login: "test", Password: "test", IPs: []string{"127.0.0.1"}}
+	validator := NewValidator(auth)
+	if validator == nil {
+		t.Error("NewValidator() should not return nil")
+	}
+}
+
+func TestNewValidator_Nil(t *testing.T) {
+	validator := NewValidator(nil)
+	if validator != nil {
+		t.Error("NewValidator() should return nil")
+	}
+}
+
+func TestNewMiddleware(t *testing.T) {
+	auth := &Auth{Login: "test", Password: "test", IPs: []string{"127.0.0.1"}}
+	middleware := NewMiddleware(auth)
+	if middleware == nil {
+		t.Error("NewMiddleware() should not return nil")
+	}
+}
+
+func TestEquals(t *testing.T) {
+	if !Equals("test", "test") {
+		t.Error("Equals() should return true")
+	}
+}
